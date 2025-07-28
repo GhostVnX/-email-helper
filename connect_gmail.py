a976ff83f6502e42f255e6490dd8652a0857246f
GhostVnX <kargboosman61@gmail.com>
2025-07-28 13:35:29 +0100

    Update connect_gmail.py

--- connect_gmail.py
+++ connect_gmail.py
@@ -1,37 +1,41 @@
-# connect_gmail.py
-
+import json
+import pickle
+import os
+import base64
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-import os.path
-import pickle
-import base64
 from email.mime.text import MIMEText
 from googleapiclient.discovery import build
+import streamlit as st
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
 def login_to_gmail():
     creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
             creds = pickle.load(token)
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_config(
+                json.loads(st.secrets["GOOGLE_CLIENT_SECRET"]),
+                SCOPES
+            )
             creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
+
+        with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
 
     return creds
 
 def send_email(creds, to, subject, message_text):
-    service = build('gmail', 'v1', credentials=creds)
-    message = MIMEText(message_text, 'html')
-    message['to'] = to
-    message['subject'] = subject
+    service = build("gmail", "v1", credentials=creds)
+    message = MIMEText(message_text, "html")
+    message["to"] = to
+    message["subject"] = subject
     raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
-    message = {'raw': raw}
-    return service.users().messages().send(userId='me', body=message).execute()
+    body = {"raw": raw}
+    return service.users().messages().send(userId="me", body=body).execute()
